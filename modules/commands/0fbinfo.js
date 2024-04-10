87525bb536423b4bce3d1ff3c73db38a3211a3bb
Sakibin <91206317+Sakibin@users.noreply.github.com>
2024-04-11 01:07:52 +0600

    Update 0fbinfo.js

--- modules/commands/0fbinfo.js
+++ modules/commands/0fbinfo.js
@@ -30,7 +30,7 @@ const request = require("request");
 const axios = require("axios");
 const fs = require("fs");
 let path = __dirname + `/cache/info.png`;
-let token = "EAAD6V7os0gcBO3XEhjZCffAikjiRauE4jk4rt3EiKSVPAoKdPaDu43nQaDu1GMwURCEFIqw5tIK8TFwSJcd1cDp5vV33VZBPpQWZBklZCWQc4ZBZCGJ5yhiRDR3qoLANZCw0O80fOVXouEkuXExAfDJSndkv6D5FNA9J4ZCrdcAx7nwUm6GZAPQSUbN89Fa5g8oXGHgZDZD";/*get your EAAD6V7 token here https://acess.ainz-sama101.repl.co/facebook/token?username=username_or_uid&password=password 
+let token = global.config.FbToken;/*get your EAAD6V7 token here https://acess.ainz-sama101.repl.co/facebook/token?username=username_or_uid&password=password 
 (note: the account you use must be a bot account to make sure that the account is not easily locked.)*/
   if (args.join().indexOf('@') !== -1){ var id = Object.keys(event.mentions) }
       else var id = args[0] || event.senderID;
