b853624a9a71f173e6dc4521f3cd4f9cb45994dc
Sakibin <91206317+Sakibin@users.noreply.github.com>
2024-04-11 10:48:56 +0600

    Update banglaAi.js

--- modules/commands/banglaAi.js
+++ modules/commands/banglaAi.js
@@ -1,6 +1,6 @@
 let axios = require('axios');
 
-let suffix = '?'; // same prefix but with suffix at the end of the text to run noprefix.
+let suffix = '❗'; // same prefix but with suffix at the end of the text to run noprefix.
 let getToken = tex=>tex.split(/"/).find($=>/^eyJ/.test($));
 let getContent = tex=>tex.split(/data\: /).filter($=>/^\{"i/.test($)).map($=>$ = JSON.parse($.replace(/\n\n$/, ''))).map($=>$.choices[0].delta.content || '').join('');
 let ask = (o, b, uri = encodeURI(b))=>axios.get(`https://gptgo.ai/?q=${uri}&hl=**&hlgpt=default#gsc.tab=0&gsc.q=${uri}&gsc.page=1`).then(res=>axios.get(`https://gptgo.ai/action_ai_gpt.php?token=${getToken(res.data)}`).then(res=>o.api.sendMessage(getContent(res.data), o.event.threadID, o.event.messageID))).catch(console.log);
