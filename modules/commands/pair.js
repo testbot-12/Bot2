93c248650bb2fc5451bb3895523e19e24d2b7746
Sakibin <91206317+Sakibin@users.noreply.github.com>
2024-04-13 00:43:32 +0600

    Update pair.js

--- modules/commands/pair.js
+++ modules/commands/pair.js
@@ -37,9 +37,11 @@ module.exports.run = async function({ api, event, args, Users, Threads, Currenci
         let Avatar2 = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
             fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
         var imglove = [];
-              imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
               imglove.push(fs.createReadStream(__dirname + "/noprefix/giflove.gif"));
+              imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
               imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
+              
+              
         var msg = {body: `Complete the pairing bar you lost 10 dollars!\your partner is of the same gender: ${gender}\nDual ratio: ${tle}\n`+namee+" "+"❤️"+" "+name, mentions: arraytag, attachment: imglove}
         return api.sendMessage(msg, event.threadID, event.messageID)
       }
