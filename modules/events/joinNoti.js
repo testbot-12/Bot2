7d651f251c96168838f37b433dd886aac5159208
Sakibin <91206317+Sakibin@users.noreply.github.com>
2024-04-14 22:30:32 +0600

    Update joinNoti.js

--- modules/events/joinNoti.js
+++ modules/events/joinNoti.js
@@ -30,9 +30,9 @@ module.exports.run = async function({ api, event }) {
 	const { join } = global.nodemodule["path"];
 	const { threadID } = event;
 	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-		api.changeNickname(`「 ${global.config.PREFIX} 」➭ ${(!global.config.BOTNAME) ? "bot" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+		api.changeNickname(`⪼ ${global.config.PREFIX} ⪻• ${(!global.config.BOTNAME) ? "bot" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
 		const fs = require("fs");
-		return api.sendMessage("", event.threadID, () => api.sendMessage({body:`▂▃▅▆𝗟𝗼𝗮𝗱𝗶𝗻𝗴...𝟭𝟬𝟬%▆▅▃▂\n⫸ 𝘾𝙤𝙣𝙣𝙚𝙘𝙩𝙚𝙙 𝙎𝙪𝙘𝙘𝙚𝙨𝙁𝙪𝙡𝙡 ⫷\n\n❐ 🄱🄾🅃 Prefix: /\n❐Example: /help\n───────────⭓\n│👤Owner:【𝗦𝗮𝗸𝗶𝗯𝗶𝗻 𝗦𝗶𝗻𝗵𝗮】\n│⚠️Don't kick this Bot!\n│✅Use /help to see CMD?\n│💟Thanks GC Admin for Add!\n│❄️I have no Spam!\n`, attachment: fs.createReadStream(__dirname + "/cache/joinmp4/rankup.gif")} ,threadID));
+		return api.sendMessage("", event.threadID, () => api.sendMessage({body:`▂▃▅▆𝗟𝗼𝗮𝗱𝗶𝗻𝗴...𝟭𝟬𝟬%▆▅▃▂\n⫸ 𝘾𝙤𝙣𝙣𝙚𝙘𝙩𝙚𝙙 𝙎𝙪𝙘𝙘𝙚𝙨𝙁𝙪𝙡𝙡 ⫷\n\n❐ 🄱🄾🅃 Prefix: /\n❐Example: /help\n───────────⭓\n│👤Owner:【𝗦𝗮𝗸𝗶𝗯𝗶𝗻 𝗦𝗶𝗻𝗵𝗮】\n│⚠️Don't kick this Bot!\n│✅Use /help to see CMD?\n│💟Thanks GC Admin for Add!\n│❄️I have no Spam!\n`, attachment: fs.createReadStream(__dirname + "/cache/leaveGif/connect.gif")} ,threadID));
 	}
 	else {
 		try {
