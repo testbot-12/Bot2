c9ade857754c7165d6e679537204f5a6c9e65f7a
Sakibin <91206317+Sakibin@users.noreply.github.com>
2024-07-08 12:10:37 +0600

    Update 0play.js

--- modules/commands/0play.js
+++ modules/commands/0play.js
@@ -1,73 +1,71 @@
-const axios = require('axios');
 const fs = require('fs');
-const ytsearch = require('youtube-search-api');
+const ytdl = require('ytdl-core');
+const Youtube = require('youtube-search-api');
+
+async function downloadMusicFromYoutube(url, path) {
+    if (!url) return 'Wrong url';
+
+    return new Promise((resolve, reject) => {
+        const stream = ytdl(url, { filter: 'audioonly' });
+        const writeStream = fs.createWriteStream(path);
+
+        stream.pipe(writeStream)
+            .on('finish', () => resolve())
+            .on('error', (error) => reject(error));
+    });
+}
 
 module.exports.config = {
+    usePrefix: true,
     name: "play",
     version: "1.0.0",
-    role: 0,
-    hasPermision: 0,
-    credits: "cliff", //api by kim
-    description: "Search and play music from music",
-    commandCategory: "music",
-    hasPrefix: false,
-    usage: "[song name]",
-    cooldowns: 5,
-    usePrefix: false,
-    usages: "[song name]",
-    cooldown: 5,
+    hasPermssion: 0,
+    credits: "D-Jukie",
+    description: "/play (music name?)",
+    commandCategory: "Media",
+    usages: "[searchMusic]",
+    cooldowns: 0
+};
+
+module.exports.convertHMS = function (value) {
+    const sec = parseInt(value, 10);
+    let hours = Math.floor(sec / 3600);
+    let minutes = Math.floor((sec - (hours * 3600)) / 60);
+    let seconds = sec - (hours * 3600) - (minutes * 60);
+    if (hours < 10) { hours = "0" + hours; }
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
+    return (hours != '00' ? hours + ':' : '') + minutes + ':' + seconds;
 };
 
 module.exports.run = async function ({ api, event, args }) {
-    const keyword = encodeURIComponent(args.join(" "));
-    const url = `https://music-api-91ji.onrender.com/music?keyword=${keyword}`;
+    if (args.length == 0 || !args) return api.sendMessage('» Please write a line!\n /play (গানের নাম?)', event.threadID, event.messageID);
 
-    if (!keyword) return api.sendMessage("Please provide the name of the song you want to search.", event.threadID, event.messageID);
+    const keywordSearch = args.join(" ");
+    const path = `${__dirname}/cache/${event.senderID}.mp3`;
+    if (fs.existsSync(path)) {
+        fs.unlinkSync(path);
+    }
 
     try {
-        var data = (await ytsearch.GetListByKeyword(keyword, false, 6)).items;
-        if (data.length === 0) {
-            api.sendMessage('No results found.', event.threadID, event.messageID);
-            return;
+        const data = (await Youtube.GetListByKeyword(keywordSearch, false, 6)).items;
+        if (data.length === 0) return api.sendMessage('No results found.', event.threadID, event.messageID);
+        
+        const result = data[0]; // Get the first result
+        const url = `https://www.youtube.com/watch?v=${result.id}`;
+        await downloadMusicFromYoutube(url, path);
+        
+        if (fs.statSync(path).size > 26214400) {
+            return api.sendMessage('The music you want to play is a bit too large. Please try a smaller one.', event.threadID, () => fs.unlinkSync(path), event.messageID);
         }
-        var name = data[0].title;
 
-        if (name) {
-            const filePath = `${__dirname}/cache/${event.senderID}.mp3`;
+        const { title, author } = await ytdl.getInfo(url).then(info => info.videoDetails);
 
-            const downloadMusic = async (url, filePath) => {
-                try {
-                    const response = await axios({
-                        method: 'get',
-                        url: url,
-                        responseType: 'stream'
-                    });
-
-                    response.data.pipe(fs.createWriteStream(filePath));
-
-                    response.data.on('end', () => {
-                        api.sendMessage({
-                            body: `🎵 | Music: ${name}\n⚡ | Flash download server by fb.me/imsakibin007`,
-                            attachment: fs.createReadStream(filePath),
-                        }, event.threadID, () => fs.unlinkSync(filePath), event.messageID);
-                    });
-
-                    response.data.on('error', (err) => {
-                        console.error('Error downloading file:', err);
-                        api.sendMessage("🚧 | An error occurred while downloading the music file.", event.threadID, event.messageID);
-                    });
-                } catch (error) {
-                    console.error('Error:', error);
-                    api.sendMessage("🚧 | An error occurred while processing your request.", event.threadID, event.messageID);
-                }
-            };
-
-            await downloadMusic(url, filePath); // Use apiUrl to directly download the mp3 file
-        } else {
-            api.sendMessage("❓ | Sorry, couldn't find the requested music on music.", event.threadID);
-        }
-    } catch (error) {
-        console.error(error);
-        api.sendMessage("🚧 | An error occurred while processing your request.", event.threadID, event.messageID);
+        return api.sendMessage({
+            body: `🎵 | Music: ${title}\n🔖 | Channel: ${author.name}\n❍ 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: @Sakibin Sinha`,
+            attachment: fs.createReadStream(path)
+        }, event.threadID, () => fs.unlinkSync(path), event.messageID);
+    } catch (e) {
+        return api.sendMessage('Server is busy now..!\n' + e, event.threadID, event.messageID);
     }
 };
